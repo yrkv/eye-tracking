6692d0c971bc43f4e3d2a762ba7288a95a4aa358
Yegor Kuznetsov <yegor@tydbits.com>
2019-09-18 16:56:43 -0700

    which python

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 
 echo before
 
-exec /home/yegor/miniconda3/bin/python -c 'import sys; print(sys.version)'
+which python
+
+export PATH="/home/yegor/miniconda3/bin:$PATH"
+
+which python
+
+python -c 'import sys; print(sys.version)'
 
 python trainer.py
 
