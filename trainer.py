b1ac43a828dfa73dc240407b0af4e8346e8b2ce6
Yegor Kuznetsov <yegor@tydbits.com>
2019-09-18 21:37:24 -0700

    mkdir

--- trainer.py
+++ trainer.py
@@ -12,7 +12,7 @@ model = network.get_model()
 
 model.fit(x_train, y_train / np.array([1920, 1080]),
                  validation_data=(x_test, y_test / np.array([1920, 1080])),
-                 batch_size=64, epochs=100,
+                 batch_size=64, epochs=10,
                  verbose=1)
 
 # send model
@@ -21,4 +21,5 @@ print('send model')
 
 #home = os.environ['JENKINS_HOME']
 #model.save('{}/eye-tracking-models/model.h5'.format(home))
+os.mkdir('models')
 model.save('models/model.h5')
