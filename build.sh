c985aad19e07f7ade5b4a3ffcfd0ae16b38fafc6
Yegor Kuznetsov <yegor@tydbits.com>
2019-09-19 21:46:56 -0700

    maybe better auto-build?

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ export PATH="/home/public/miniconda3/bin:$PATH"
 
 python trainer.py
 
-
 # by this point, trainer.py should have added whatever to this directory
 
-git rm --cached -r .
-git add *.h5
+cp *.h5 $JENKINS_HOME/eye-tracking-models
+git add .
 git commit -a -m "auto-push: $BUILD_NUMBER"
+git push
