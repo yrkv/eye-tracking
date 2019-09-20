8a12ee1da437f6de30fa38898ac3e8f9839e003f
Yegor Kuznetsov <yegor@tydbits.com>
2019-09-19 21:38:45 -0700

    more data

--- trainer.py
+++ trainer.py
@@ -12,7 +12,7 @@ model = network.get_model()
 
 model.fit(x_train, y_train / np.array([1920, 1080]),
                  validation_data=(x_test, y_test / np.array([1920, 1080])),
-                 batch_size=64, epochs=100,
+                 batch_size=64, epochs=200,
                  verbose=1)
 
 model.save('model.h5')
